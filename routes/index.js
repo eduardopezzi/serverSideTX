176e46a158f1daaaa7dd5bc6b5f6c6130548f7a8
Eduardo Pezzi <eduardopezzi@Eduardos-MacBook-Pro.local>
2020-06-17 08:47:13 -0400

    "get nonce included"

--- routes/index.js
+++ routes/index.js
@@ -4,69 +4,74 @@ var Tx = require("ethereumjs-tx").Transaction;
 var Web3 = require("web3");
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-const accounts = web3.eth.getAccounts();
-var flipCoinContract = new web3.eth.Contract(
-  [
-    {
-      constant: false,
-      inputs: [],
-      name: "bet",
-      outputs: [],
-      payable: true,
-      stateMutability: "payable",
-      type: "function"
-    },
-    {
-      inputs: [],
-      payable: true,
-      stateMutability: "payable",
-      type: "constructor"
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: "maxBet",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256"
-        }
-      ],
-      payable: false,
-      stateMutability: "view",
-      type: "function"
-    }
-  ],
-  "0xC48f9fB120bB8BeF89E7BEf8B9eBc26F5021F292"
-);
 
-var account = "0x95af85Cb9efD3f2B3ba288402428f9220137EC7A";
+router.get("/", async function (req, res, next) {
+  const accounts = await web3.eth.getAccounts();
+  var flipCoinContract = new web3.eth.Contract(
+    [
+      {
+        constant: false,
+        inputs: [],
+        name: "bet",
+        outputs: [],
+        payable: true,
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [],
+        payable: true,
+        stateMutability: "payable",
+        type: "constructor",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "maxBet",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    "0xC48f9fB120bB8BeF89E7BEf8B9eBc26F5021F292"
+  );
 
-var privateKey = new Buffer.from(
-  "9EA8819710349B9DA83A939FB025BE28C235F06140E9CDBFCE1173B4A1DFFEDF",
-  "hex"
-);
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  var account = "0x81bfF584c0b449c7Bf9BAc47aC17d2ECe397b3eA";
+
+  var privateKey = new Buffer.from("", "hex");
+
+  let _nonce = await web3.eth.getTransactionCount(account);
+  _nonce = web3.utils.toHex(_nonce);
+
+  var data = flipCoinContract.methods.maxBet().encodeABI();
+  var rawTx = {
+    nonce: _nonce,
+    gasPrice: "0x09184e72a000", // increase gas if send error
+    gasLimit: "0x6710",
+    to: "0x0000000000000000000000000000000000000000",
+    value: "0x00",
+    data: data,
+  };
+  var tx = new Tx(rawTx);
+  tx.sign(privateKey);
+
+  var serializedTx = tx.serialize();
+  const _receipt = await web3.eth
+    .sendSignedTransaction("0x" + serializedTx.toString("hex"))
+    .on("receipt", console.log);
+
+  /* GET home page. */
+
+  res.render("index", {
+    title: "Express",
+    receipt: JSON.stringify(_receipt),
+  });
 });
-
-var data = flipCoinContract.methods.maxBet().encodeABI();
-var rawTx = {
-  nonce: "0x0748",
-  gasPrice: "0x09184e72a000", // increase gas if send error
-  gasLimit: "0x6710",
-  to: "0x0000000000000000000000000000000000000000",
-  value: "0x00",
-  data: data
-};
-var tx = new Tx(rawTx);
-tx.sign(privateKey);
-
-var serializedTx = tx.serialize();
-web3.eth
-  .sendSignedTransaction("0x" + serializedTx.toString("hex"))
-  .on("receipt", console.log);
-
 module.exports = router;
